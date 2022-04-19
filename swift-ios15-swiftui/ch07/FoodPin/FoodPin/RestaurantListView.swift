ae3551148ec9204c15b66fbd1fc1142034f2e11d
dvnik147 <dvnik147@hotmail.com>
2022-04-19 17:22:37 +0800

    finish homework

--- swift-ios15-swiftui/ch07/FoodPin/FoodPin/RestaurantListView.swift
+++ swift-ios15-swiftui/ch07/FoodPin/FoodPin/RestaurantListView.swift
@@ -13,27 +13,55 @@ struct RestaurantListView: View {
     
     var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkee", "posatelier", "bourkestreetbakery", "haigh", "palomino", "upstate", "traif", "graham", "waffleandwolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "cask"]
     
+    // 作業 #1 ：更新位置與型態標籤
+    var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York", "New York", "New York", "New York", "New York", "New York", "New York", "London", "London", "London", "London"]
+
+    var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causual Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood", "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "Latin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
+    
     var body: some View {
         List {
             ForEach(restaurantNames.indices, id: \.self) { index in
                 
-                HStack(alignment: .top, spacing: 20) {
+//                HStack(alignment: .top, spacing: 20) {
+//                    Image(restaurantImages[index])
+//                        .resizable()
+//                        .frame(width: 120, height: 118)
+//                        .cornerRadius(20)
+//
+//                    VStack(alignment: .leading) {
+//                        Text(restaurantNames[index])
+//                            .font(.system(.title2, design: .rounded))
+//                        //作業 #1
+//                        Text(restaurantTypes[index])
+//                            .font(.system(.body, design: .rounded))
+//                        //作業 #1
+//                        Text(restaurantLocations[index])
+//                            .font(.system(.subheadline, design: .rounded))
+//                            .foregroundColor(.gray)
+//                    }
+//                }
+                //作業#2：重新設計列的佈局
+                VStack(alignment: .leading, spacing: 10) {
                     Image(restaurantImages[index])
                         .resizable()
-                        .frame(width: 120, height: 118)
+                        .frame(height: 200)
                         .cornerRadius(20)
                     
                     VStack(alignment: .leading) {
                         Text(restaurantNames[index])
                             .font(.system(.title2, design: .rounded))
-                        Text("Type")
+                        //作業 #1
+                        Text(restaurantTypes[index])
                             .font(.system(.body, design: .rounded))
-
-                        Text("Location")
+                        //作業 #1
+                        Text(restaurantLocations[index])
                             .font(.system(.subheadline, design: .rounded))
                             .foregroundColor(.gray)
                     }
+                    .padding(.horizontal)
+                    .padding(.bottom)
                 }
+                .padding()
                 
                 
             }
