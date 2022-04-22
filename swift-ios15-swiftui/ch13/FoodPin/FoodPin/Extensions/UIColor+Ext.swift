361388b5ce15df44567f652e4adea97aea0f6efc
dvnik147 <dvnik147@hotmail.com>
2022-04-22 11:03:24 +0800

    finish ch13

---
+++ swift-ios15-swiftui/ch13/FoodPin/FoodPin/Extensions/UIColor+Ext.swift
@@ -0,0 +1,18 @@
+//
+//  UIColor+Ext.swift
+//  FoodPin
+//
+//  Created by Trixie Lulamoon on 2022/4/22.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    convenience init(red: Int, green: Int, blue: Int) {
+        let redValue = CGFloat(red) / 255.0
+        let greenValue = CGFloat(green) / 255.0
+        let blueValue = CGFloat(blue) / 255.0
+        self.init(red: redValue, green: greenValue, blue: blueValue, alpha: 1.0)
+    }
+}
