9176393edbe912fb28ddb2ca4a28168e94cfa254
dvnik147 <dvnik147@hotmail.com>
2022-05-04 17:19:47 +0800

    finish ch10

---
+++ master-swiftui/ch10/SwiftUIList/SwiftUIList/SwiftUIListApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIListApp.swift
+//  SwiftUIList
+//
+//  Created by Trixie Lulamoon on 2022/5/4.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ master-swiftui/ch10/SwiftUIListHomeWork/SwiftUIListHomeWork/SwiftUIListHomeWorkApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIListHomeWorkApp.swift
+//  SwiftUIListHomeWork
+//
+//  Created by Trixie Lulamoon on 2022/5/4.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIListHomeWorkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
