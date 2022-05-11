a7f57d4ff91f2802e9be34ce8ec0c37aec7903d7
dvnik147 <dvnik147@hotmail.com>
2022-05-11 13:51:48 +0800

    finish ch21

---
+++ master-swiftui/ch21/SwiftUIKivaLoan/SwiftUIKivaLoan/SwiftUIKivaLoanApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIKivaLoanApp.swift
+//  SwiftUIKivaLoan
+//
+//  Created by Trixie Lulamoon on 2022/5/11.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIKivaLoanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
