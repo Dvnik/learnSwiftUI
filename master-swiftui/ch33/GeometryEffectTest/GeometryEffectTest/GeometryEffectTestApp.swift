68d6f22454cb6c6776dd85a686fb9e5f3341df09
dvnik147 <dvnik147@hotmail.com>
2022-05-18 15:15:14 +0800

    finish ch34

---
+++ master-swiftui/ch33/GeometryEffectTest/GeometryEffectTest/GeometryEffectTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeometryEffectTestApp.swift
+//  GeometryEffectTest
+//
+//  Created by Trixie Lulamoon on 2022/5/18.
+//
+
+import SwiftUI
+
+@main
+struct GeometryEffectTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
