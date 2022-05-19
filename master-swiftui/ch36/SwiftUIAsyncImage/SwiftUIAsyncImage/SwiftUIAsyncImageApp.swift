8df3e45f24972e7ed439a30b57efb7c9a5b05141
dvnik147 <dvnik147@hotmail.com>
2022-05-19 11:22:28 +0800

    finish ch36

---
+++ master-swiftui/ch36/SwiftUIAsyncImage/SwiftUIAsyncImage/SwiftUIAsyncImageApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAsyncImageApp.swift
+//  SwiftUIAsyncImage
+//
+//  Created by Trixie Lulamoon on 2022/5/19.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAsyncImageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
